365e4fd531db5520500c5c0e150b648b445f094b
lizhaoshuai <lizhaoshuai@hotmail.com>
2020-06-28 18:18:32 +0800

    构建RESTful Web服务

---
+++ src/main/java/com/example/JavaStudyGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudyGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStudyGithubApplication.class, args);
+	}
+
+}
